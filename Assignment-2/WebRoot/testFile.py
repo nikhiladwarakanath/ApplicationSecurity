c9ca44fa54ac459dfa0242818dcc37cf3343bff2
nikhiladwarakanath <nikhila.dwarakanath@nyu.edu>
2019-12-09 22:23:18 -0500

    added Test cases in the testFile

--- Assignment-2/WebRoot/testFile.py
+++ Assignment-2/WebRoot/testFile.py
@@ -56,3 +56,47 @@ def test_Logout(testClient):
     res = testClient.get("/logout")
     assert res.status_code == 200 #since user session is not available
     # assert res.result == "none" 
+
+##################################################################################################################
+##################################################################################################################
+# New Test Cases for Assignment -3
+def test_HistoryGet(testClient):
+    # print(testClient)
+    res = testClient.get("/history")
+    assert res.status_code == 200 #since it provides the GET for any logged user
+    # assert res.result == "none" 
+
+def test_HistoryPost(testClient):
+    try:
+        res = testClient.post('/history', {'userquery':'hi'})
+    except ValueError :
+        print("error")
+        assert True
+        # The flow should enter exception block since admin check is missing and csrf token is not available
+
+def test_HistoryQueryGet(testClient):
+    res=testClient.get("/history/query1")
+    assert res.status_code == 200 # test if end point returns a webpage
+
+def test_HistoryQueryGetSecond(testClient):
+    res=testClient.get("/history/query10988")
+    assert res.status_code == 200 # test if end point returns a webpage
+
+
+
+def test_LoginHistoryGet(testClient):
+    # only admin can access this page, hence it should return 404 status code
+    res = testClient.get("/login_history")
+    assert res.status_code == 404
+
+
+def test_LoginHistoryPost(testClient):
+    try:
+        res = testClient.post("/login_history",{'userid':'harry'})
+    except ValueError:
+        print("error")
+        assert True
+        # The flow should enter exception block since admin check is missing and csrf token is not available, only admins can send a post request to login history
+
+
+# All test cases are passing
